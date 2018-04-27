3b32732b68516d19dce3cf67a1e3f8d9ab4c5e39
deepthi dodla <deepthidodla@deepthis-Air.T-mobile.com>
2018-04-27 16:53:12 -0500

    FIRST COMMIT

---
+++ simple-temp.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const axios = require('axios');
+const port = 8080;
+const app = express();
+
+
+let num = Math.floor(Math.random() * 10000);
+const version = process.env.VERSION;
+
+app.get('/', (request, response) => {
+  console.log(`From server ${num} Received request for URL: ${request.url} version ${version}`)
+  response.writeHead(200);
+  response.end(`Hello World! from server ${num} version ${version}`);
+  
+})
+
+app.get('/health', (request, response) => {
+  console.log(`Health check instance ${num} Received request for URL: ${request.url} version ${version}`)
+  response.writeHead(200);
+  response.end(`I am healthy! from server ${num} version ${version}`);
+  
+})
+
+
+app.listen(port, (err) => {
+  console.log("started server");
+  if(err)
+    return console.log(`something happened on startup with text` );
+
+});
