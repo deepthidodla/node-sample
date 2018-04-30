97ddd784660852c211310a451d1ce51160542cdb
deepthi dodla <deepthidodla@deepthis-Air.T-mobile.com>
2018-04-29 22:04:52 -0500

    file

--- deploy-nodejs.sh
+++ deploy-nodejs.sh
@@ -1,5 +1,6 @@
 
 # deploy goal is copy build to remote server and replace and restart
+sudo - ec2-user
 cd /home/ec2-user
 ssh -i mykey ec2-user@52.55.213.77 <<EOF        
  cd /home/ec2-user/tempfiles 
